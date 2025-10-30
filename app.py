ed19a7db5137241a9005b4f8c064ea950b471df3
Maksym <maximshnaider@gmail.com>
2025-10-31 00:21:32 +0100

    feat: setup FastAPI + Streamlit project with Ruff and pre-commit

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI
-import uvicorn
+import streamlit as st
+import requests
 
-app = FastAPI()
+st.title("Calculator")
 
-@app.get("/")
-def home():
-    return {"message": "хелоу"}
+a = st.number_input("First", 0)
+b = st.number_input("Second", 0)
 
-@app.get("/sum")
-def get_sum(a: int, b: int):
-    return {"result": a + b}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+if st.button("Go"):
+    r = requests.get("http://127.0.0.1:8000/sum", params={"a": a, "b": b})
+    st.write("Result:", r.json()["result"])
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import streamlit as st
-import requests
+from fastapi import FastAPI
 
-st.title("Calculator")
+app = FastAPI()
 
-a = st.number_input("First", 0)
-b = st.number_input("Second", 0)
 
-if st.button("Go"):
-    r = requests.get("http://127.0.0.1:8000/sum", params={"a": a, "b": b})
-    st.write("Result:", r.json()["result"])
+@app.get("/sum")
+def get_sum(a: int, b: int):
+    result = a + b
+    return {"result": result}
